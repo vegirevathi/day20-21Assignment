f6ca0a402bf6e0052fde78b2ff1574f0f982fccd
vegirevathi <vegirevathi@gmail.com>
2020-05-23 18:11:51 +0530

    Adding max working hrs condition in UC6

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,14 +1,16 @@
 public class EmpWageComputation {
-   public static void main(String[] args) {
-      System.out.println("Welcome to Employee Wage Computation");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int NUM_OF_WORKING_DAYS = 20;
-		int empHrs = 0, empWage = 0, totalEmpWage = 0;
-		for (int day = 0; day <= NUM_OF_WORKING_DAYS; day++){
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 20;
+	public static final int MAX_WORKING_HRS = 100;
+
+	public static void main(String[] args) {
+		int empHrs = 0, empWage = 0, totalEmpWage = 0, totalWorkingDays = 0, totalEmpHrs = 0;
+		while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 		int empCheck = (int)(Math.random() * 10) %3;
-		switch ( empCheck) {
+		switch (empCheck) {
 				case 1:
 					System.out.println("Employee is Present");
 					empHrs = 8;
