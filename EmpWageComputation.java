1f9d3add53d712ce14ee703876c3de7a559e8b8d
vegirevathi <vegirevathi@gmail.com>
2020-05-23 17:15:26 +0530

    Adding Part time employee wage in UC3

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,13 +2,17 @@ public class EmpWageComputation {
    public static void main(String[] args) {
       System.out.println("Welcome to Employee Wage Computation");
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random() * 10) %2;
+		double empCheck = Math.floor(Math.random() * 10) %3;
 		if ( empCheck == IS_FULL_TIME )
 			System.out.println("Employee is Present");
 			empHrs = 8;
+		else if ( empCheck == IS_PART_TIME)
+			System.out.println("Employee is Present");
+			empHrs = 4;
 		else
 			System.out.println("Employee is Absent");
 			empHrs = 0;
